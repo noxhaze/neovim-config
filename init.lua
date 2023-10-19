2fc1f3c724461d56078d57b5dfa4d06eda218aa9
noxhaze <noxhaze@proton.me>
2023-10-19 10:32:15 -0400

    Start actual git project

---
+++ init.lua
@@ -0,0 +1,76 @@
+vim.g.mapleader = " "
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+vim.opt.tabstop = 4;
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.termguicolors = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.api.nvim_replace_termcodes('<C-\\><C-N>', true, true, true)
+
+local opts = {
+    noremap = true,
+    silent = true,
+}
+
+vim.keymap.set('i', "<C-h>", "<Left>", opts);
+vim.keymap.set('i', "<C-j>", "<Down>", opts);
+vim.keymap.set('i', "<C-k>", "<Up>", opts);
+vim.keymap.set('i', "<C-l>", "<Right>", opts);
+
+vim.keymap.set('i', "<C-e>", "<End>", opts);
+
+vim.keymap.set('n', "<C-h>", "<C-w>h", opts);
+vim.keymap.set('n', "<C-j>", "<C-w>j", opts);
+vim.keymap.set('n', "<C-l>", "<C-w>l", opts);
+vim.keymap.set('n', "<C-k>", "<C-w>k", opts);
+
+vim.keymap.set("n", "<Leader>FF", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>", opts);
+vim.keymap.set("n", "<Leader>fs", "<cmd>Telescope live_grep theme=ivy<cr>", opts);
+vim.keymap.set("n", "<Leader>ff", "<cmd>Telescope find_files<cr>", opts);
+vim.keymap.set("n", "<Leader>fb", "<cmd>Telescope buffers<cr>", opts);
+vim.keymap.set("n", "<C-p>", ":lua require('telescope').extensions.project.project{}<CR>", opts);
+
+vim.keymap.set("n", "<C-n>", ":e ./<CR>", opts);
+
+vim.keymap.set("n", "<Leader>h", ":lua require('nvterm.terminal').new 'horizontal'<CR>", opts)
+vim.keymap.set("n", "<Leader>v", ":lua require('nvterm.terminal').new 'vertical'<CR>", opts)
+vim.keymap.set("n", "<A-h>", ":lua require('nvterm.terminal').toggle 'horizontal'<CR>", opts);
+vim.keymap.set("n", "<A-v>", ":lua require('nvterm.terminal').toggle 'vertical'<CR>", opts);
+vim.keymap.set("t", "<C-x>", "<C-\\><C-N>", opts)
+vim.keymap.set("t", "<A-h>", "<C-\\><C-N> :lua require('nvterm.terminal').toggle 'horizontal'<CR>", opts)
+vim.keymap.set("t", "<A-v>", "<C-\\><C-N> :lua require('nvterm.terminal').toggle 'vertical'<CR>", opts)
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = require("plugins")
+require("lazy").setup(plugins, opts)
+require("lsp")
+
+vim.cmd("colorscheme tomorrow-night")
+
---
+++ lua/plugins.lua
@@ -0,0 +1,156 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+    },
+
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            require("nvim-cmp")
+        end,
+
+        lazy = false,
+    },
+
+    {
+        "hrsh7th/cmp-nvim-lsp",
+
+        dependencies = {
+            "nvim-cmp",
+        },
+    },
+
+    {
+        "hrsh7th/cmp-buffer",
+
+        dependencies = {
+            "nvim-cmp",
+        },
+    },
+
+    {
+        "hrsh7th/cmp-path",
+
+        dependencies = {
+            "nvim-cmp",
+        },
+    },
+
+    {
+        "hrsh7th/cmp-cmdline",
+
+        dependencies = {
+            "nvim-cmp",
+        },
+    },
+
+    {
+        "L3MON4D3/LuaSnip",
+    },
+
+    {
+        "saadparwaiz1/cmp_luasnip",
+    },
+
+
+    -- end of cmp
+
+    {
+        "williamboman/mason.nvim",
+        lazy = false
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+    },
+
+    {
+        "squarefrog/tomorrow-night.vim",
+    },
+
+    {
+        "nvim-lua/plenary.nvim",
+    },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            extensions_list = { "project" },
+        },
+        extension = {
+            project = {
+                hidden_files = true,
+                theme = "dropdown",
+                order_by = "asc",
+                search_by = "title",
+            },
+        },
+
+        lazy = false,
+
+    },
+
+    {
+        "nvim-telescope/telescope-project.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+        },
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            vim.cmd("TSUpdate")
+        end,
+        lazy = false,
+    },
+
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+        lazy = false,
+    },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        lazy = false,
+        dependencies = {
+            "squarefrog/tomorrow-night.vim",
+        },
+        config = function ()
+            require("lualine").setup({
+                options = {
+                    theme = "base16",
+                },
+            })
+        end
+    },
+
+    {
+        "NvChad/nvterm",
+        config = function ()
+            require("nvterm").setup()
+        end
+    },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function ()
+            require("gitsigns").setup()
+        end
+    },
+
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function ()
+            require("bufferline").setup{}
+        end
+    },
+}
