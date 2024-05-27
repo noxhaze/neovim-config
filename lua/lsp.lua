7fdcffc0253b8b4b2c939146b6b41abb6e66a28b
noxhaze <noxhaze@proton.me>
2024-05-27 15:45:22 -0400

    New theme + lua_lsp update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -34,6 +34,28 @@ require('lspconfig').clangd.setup({
   cmd = { 'clangd', '-header-insertion=never' }
 })
 
+require 'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        }
+      }
+    })
+  end,
+}
+
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 cmp.setup({
